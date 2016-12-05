d33659ab55d6dc32b4c7f55b3696d67c03a07d92
blondabambi <ebba@aniansson.se>
2016-12-05 20:55:58 +0100

    added missing gems

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,14 +1,13 @@
+require 'rspec'
 require 'capybara/rspec'
+require 'middleman-core'
+require 'middleman-core/rack'
+require 'middleman-livereload'
 
-RSpec.configure do |config|
-  config.expect_with :rspec do |expectations|
-    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
-  end
+middleman_app = ::Middleman::Application.new
 
-  config.mock_with :rspec do |mocks|
-    mocks.verify_partial_doubles = true
-  end
-
-  config.shared_context_metadata_behavior = :apply_to_host_groups
-  config.include Capybara::DSL
+Capybara.app = ::Middleman::Rack.new(middleman_app).to_app do
+  set :root, File.expand_path(File.join(File.dirname(__FILE__), '..'))
+  set :environment, :development
+  set :show_exceptions, false
 end
