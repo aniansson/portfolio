fa42c16eac305bf6747b69a88651bb9b1f173524
blondabambi <ebba@aniansson.se>
2016-12-05 16:20:40 +0100

    add missing gemfiles

--- Gemfile
+++ Gemfile
@@ -11,3 +11,7 @@ gem 'tzinfo-data', platforms: [:mswin, :mingw, :jruby]
 # Middleman Gems
 gem 'middleman', '>= 4.0.0'
 gem 'middleman-livereload'
+
+group :development, :test do
+  gem 'capybara'
+end
---
+++ spec/features/index_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe 'index', type: :feature do
+
+  before do
+    visit '/'
+  end
+
+it 'displays project list' do
+  expect(page).to have_css '.projects'
+  within '.projects' do
+    expect(page).to have_content 'My First Website'
+    expect(page).to have_content 'FizzBuzz'
+  end
+end
+end
